8d5bb1194ec3b54066f97dbe9f526cd848595a5c
Farhana Sultana <farhanaSmrity@gmail.com>
2025-07-07 14:50:37 +0600

    add link & search data

---
+++ cypress/e2e/test1.cy.js
@@ -0,0 +1,8 @@
+/// <reference tpes = "cypress"
+
+it('Google Search',()=>{
+    cy.visit('https://www.google.com/');
+    cy.get('#APjFqb'), {timeout:5000}.type('Automation step by step{Enter}')
+    //cy.contains('Google Search').click
+    cy.contains('Videos').click
+})
